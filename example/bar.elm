7bbedf843f3f7281a7317521172efe3bc703a895
Alex <s3ek.mail@gmail.com>
2017-12-24 23:40:00 +0300

    add second chart to example

--- example/bar.elm
+++ example/bar.elm
@@ -17,6 +17,7 @@ main =
 type alias Model =
     { hello : String
     , latte : LatteModel.Model
+    , latte2 : LatteModel.Model
     }
 
 
@@ -36,6 +37,16 @@ model =
                 ]
             , title = "Moons of Jupiter"
             }
+    , latte2 =
+        latteMake 640 200 <|
+            { chart = Bar
+            , labels =
+                [ "Io", "Europa", "Ganymede", "Callisto" ]
+            , datasets =
+                [ Dataset "Mass" [ 8931900, 4800000, 14819000, 10759000 ]
+                ]
+            , title = "Moons of Jupiter"
+            }
     }
 
 
@@ -45,6 +56,7 @@ model =
 
 type Msg
     = Latte LatteMsg.Msg
+    | LatteSecond LatteMsg.Msg
 
 
 update : Msg -> Model -> Model
@@ -52,6 +64,9 @@ update msg model =
     case msg of
         Latte msg ->
             { model | latte = (latteUpdate msg model.latte) }
+
+        LatteSecond msg ->
+            { model | latte2 = (latteUpdate msg model.latte2) }
 
 
 
@@ -62,6 +77,7 @@ view : Model -> Html Msg
 view model =
     div []
         [ Html.map Latte (latteDraw model.latte)
+        , Html.map LatteSecond (latteDraw model.latte2)
         , ol []
             [ strong [] [ text "Todo:" ]
             , li [] [ s [] [ text "Aspect Bars by X" ] ]
