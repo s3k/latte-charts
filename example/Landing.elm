2d8c18549a98f494079af6e3f4a78170087bc141
Alex <s3ek.mail@gmail.com>
2018-01-31 22:59:56 +0300

    WIP: Site boilerplate

--- example/Landing.elm
+++ example/Landing.elm
@@ -1,11 +1,12 @@
 module Landing exposing (..)
 
-import Html exposing (Html, div, li, ol, s, strong, text, h4, p)
+import Html exposing (Html, div, li, ol, s, strong, text, h1, h2, h3, h4, p, a, header)
 import Html.Attributes exposing (class, style)
 import Latte exposing (..)
 import Latte.Model as LatteModel exposing (Chart(..), Dataset)
 import Latte.Msg as LatteMsg
 import Window
+import Markdown
 
 
 main =
@@ -22,8 +23,7 @@ main =
 
 
 type alias Model =
-    { hello : String
-    , latte : LatteModel.Model
+    { latte : LatteModel.Model
     }
 
 
@@ -31,19 +31,17 @@ init : ( Model, Cmd Msg )
 init =
     let
         model =
-            { hello =
-                "Hello, this is a Latte Chart example"
-            , latte =
-                latteMake 400 200 <|
+            { latte =
+                latteMake 950 200 <|
                     { chart = Bar
                     , labels =
-                        [ "Io", "Europa", "Ganymede", "Callisto" ]
+                        [ "Io", "Europa", "Ganymede", "Callisto", "Fake" ]
                     , datasets =
-                        [ Dataset "Mass" [ 8931900, 4800000, 14819000, 10759000 ]
-                        , Dataset "Diameter" <| List.map (\n -> n * 1000) [ 3660.0, 3121.6, 5262.4, 4820.6 ]
-                        , Dataset "Semi-Major" [ 421700, 671034, 1070412, 1882709 ]
+                        [ Dataset "Mass" [ 8931900, 4800000, 14819000, 10759000, 10759000 ]
+                        , Dataset "Diameter" <| List.map (\n -> n * 1000) [ 3660.0, 3121.6, 5262.4, 4820.6, 4000 ]
+                        , Dataset "Semi-Major" [ 421700, 671034, 1070412, 1882709, 1882709 ]
                         ]
-                    , title = "Moons of Jupiter"
+                    , title = "Biggest Moons of Jupiter"
                     }
             }
     in
@@ -82,37 +80,88 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div
-        [ class "container content animated fadeIn"
-        ]
-        [ div [ class "row" ]
-            [ div [ class "six columns" ]
-                [ strong []
-                    [ text "Moons of Jupiter" ]
-                , p
-                    []
-                    [ text aboutJupiter ]
-                ]
-            , div [ class "six columns content-item" ]
-                [ -- h4 [ class "center" ] [ text "Multiple Bar Charts" ]
-                  Html.map Latte (latteDraw model.latte)
+    div []
+        [ header []
+            [ div [ class "container" ]
+                [ div [ class "row" ]
+                    [ div [ class "column" ]
+                        [ div [ class "logo animated bounceInDown" ] []
+                        , p [ class "subtitle animated fadeIn" ]
+                            [ text "Library for creating charts in Elm language"
+                            ]
+                        ]
+                    ]
                 ]
             ]
-        , div [ class "row" ]
-            [ div [ class "six columns" ]
-                [ strong []
-                    [ text "Moons of Jupiter" ]
-                , p
-                    []
-                    [ text aboutJupiter ]
+        , div [ class "section" ]
+            [ div
+                [ class "container content animated fadeIn"
                 ]
-            , div [ class "six columns content-item" ]
-                [ -- h4 [ class "center" ] [ text "Multiple Bar Charts" ]
-                  Html.map Latte (latteDraw model.latte)
+                [ mainChart model
+                , installView model
+                  -- , jupiterView model
                 ]
             ]
         ]
 
 
-aboutJupiter =
-    "There are 69 known moons of Jupiter. This gives Jupiter the largest number of moons with reasonably stable orbits of any planet in the Solar System"
+
+-- View Sections
+
+
+mainChart : Model -> Html Msg
+mainChart model =
+    div [ class "row" ]
+        [ h2 [ class "center" ]
+            [ text "Moons of Jupiter" ]
+        , div
+            [ class "content-item" ]
+            [ Html.map Latte (latteDraw model.latte)
+            , div [ class "center btn-group" ]
+                [ a [ class "btn active" ] [ text "Multiple Bar" ]
+                , a [ class "btn" ] [ text "Line" ]
+                , a [ class "btn" ] [ text "Percentage" ]
+                , a [ class "btn" ] [ text "Scatter" ]
+                , a [ class "btn" ] [ text "Simple Bar" ]
+                ]
+            ]
+        ]
+
+
+jupiterView : Model -> Html Msg
+jupiterView model =
+    div [ class "row" ]
+        [ div [ class "six columns" ]
+            [ h1 []
+                [ text "Moons of Jupiter" ]
+            , p
+                []
+                [ text
+                    """
+                      There are 69 known moons of Jupiter.
+                      This gives Jupiter the largest number of moons with reasonably stable orbits of any planet in the Solar System.
+                    """
+                ]
+            , p [] [ text "Compare the biggest on latte bar chart!" ]
+            ]
+        , div [ class "six columns content-item" ]
+            [ Html.map Latte (latteDraw model.latte)
+            ]
+        ]
+
+
+installView : Model -> Html Msg
+installView model =
+    div [ class "row" ]
+        [ Markdown.toHtml [ class "content" ] """
+
+## Installation
+
+Add package Latte Chart package
+
+```
+$ elm package install s3k/latte
+```
+
+"""
+        ]
